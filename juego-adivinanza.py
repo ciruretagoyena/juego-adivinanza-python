f19acec25c03419e9a269e942976f715eebf48cb
ciruretagoyena <ciruretagoyena@gmail.com>
2024-12-01 20:00:21 -0300

    Hicimos un juego de adivinanza en Python

---
+++ juego-adivinanza.py
@@ -0,0 +1,19 @@
+
+import random
+
+numero_secreto = random.randint(1,100)
+adivinado = False
+
+print("¡Bienvenido al juego de adivinar el número secreto!")
+
+while not adivinado:
+    entrada = input("Introduce un número del 1 al 99: ")
+    numero = int(entrada)
+
+    if numero == numero_secreto:
+        print("¡Felicitaciones haz adivinado el número secreto!")
+        adivinado = True
+    elif numero < numero_secreto:
+        print("El número es mayor al ingresado")
+    else:
+        print("El número es menor al ingresado")
